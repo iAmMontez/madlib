bb623e472495b12e8e2334508866b39c4a6d44d5
iAmMontez <mcaldw8949@greenvilleschools.us>
2017-05-24 14:11:06 -0400

    Add files via upload

---
+++ story_maker.js
@@ -0,0 +1,21 @@
+function makeStory() {
+    // get form values (2 points)
+    var noun1 = document.getElementById('noun1').value;
+    var noun2 = document.getElementById('noun2').value;
+    var verb1 = document.getElementById('verb1').value;
+
+
+    // Set title of story. Use at least one form value in the title. (2 points)
+    var title = "";
+
+
+    // Build story. you can add as many paragraphs as you like. (3 points)
+    var paragraph1 = "";
+    var paragraph2 = "";
+    var paragraph3 = "";
+
+
+    // Display story by putting title & paragraphs into appropriate divs. (3 points)
+
+
+}
